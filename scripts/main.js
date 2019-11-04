3e942ba8f8da4b74bcc8c997ecbd002472df62cf
tineprincic1 <57317813+tineprincic1@users.noreply.github.com>
2019-11-04 20:41:35 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc !== 'images/rar-logo.jpg') {
+      myImage.setAttribute ('src','images/rar-logo.jpg');
+    } else {
+      myImage.setAttribute ('src','images/slika-2.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
